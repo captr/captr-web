6d725996c35ace6eec71d1ebf82272563d1321ac
Antonio Scandurra <as_cii@hotmail.it>
2014-02-17 20:26:29 +0100

    Add pending steps for authentication.feature

---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,39 @@
+Given(/^I am currently not signed in$/) do
+  pending
+end
+
+Given(/^I am a new user$/) do
+  pending
+end
+
+When(/^I successfully authenticate with the social network$/) do
+  pending
+end
+
+When(/^I choose an unique username$/) do
+  pending
+end
+
+Then(/^I should see my account$/) do
+  pending
+end
+
+When(/^I choose an existing username$/) do
+  pending
+end
+
+Then(/^I should be prompted to try another username$/) do
+  pending
+end
+
+Given(/^I am an existing user$/) do
+  pending
+end
+
+When(/^I cancel authentication with the social network$/) do
+  pending
+end
+
+Then(/^I should be informed that an account was not created$/) do
+  pending
+end
